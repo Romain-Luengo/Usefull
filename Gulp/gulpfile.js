a79cc5f9d45940fa2ceb03b9302752cc8c39a8e3
Aewil <luengo.romain@gmail.com>
2015-11-04 23:23:45 +0100

    V1.00

---
+++ Gulp/gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    less = require('gulp-less'),
+    path = require('path'),
+    coffee = require('gulp-coffee'),
+    plumber = require('gulp-plumber'),
+    concat = require('gulp-concat');
+    notifier = require('node-notifier');
+
+gulp.task('watch', function (cb) {
+    watch('less/**/*.less', function () {
+      gulp.src('less/**/*.less')
+        .pipe(plumber())
+        .pipe(less({
+          paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./css'));
+
+      notifier.notify({
+      'title': 'LESS',
+      'message': 'LESS COMPILE'
+      });
+    });
+
+    watch('coffee/**/*.coffee', function () {
+      gulp.src('coffee/**/*.coffee')
+        .pipe(plumber())
+        .pipe(coffee())
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./js'));
+
+      notifier.notify({
+      'title': 'COFFEE',
+      'message': 'COFFEE COMPILE'
+      });
+    });
+});
